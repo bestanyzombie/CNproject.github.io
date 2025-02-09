1efa985c0206d158fd2aab049f6e064b7a0dbae7
hungnguyenrhythm <hungcqnguyen@gmail.com>
2025-02-09 11:21:43 -0500

    Update ip.php

--- ip.php
+++ ip.php
@@ -8,6 +8,24 @@
         } else {
             $ipAddress = $_SERVER['REMOTE_ADDR'];
         }
+        $host = gethostname();
+        $server = getservbyport(22, "tcp");
+        echo "<h1>Results</h1>";
+        echo "<p>Host name: " . $host . "</p>";
         echo "<p>IP Address: " . $ipAddress . "</p>";
+        echo "<p>Server type: " . $server . "</p>";
     }
 ?>
+<html>
+    <head>
+    <style>
+      h1 {text-align: center;}
+      p {text-align: center;}
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Computer Network Website Project</title>
+    <link rel="stylesheet" href="./style.css">
+    </head>
+</html>
