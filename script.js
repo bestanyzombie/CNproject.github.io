96080f747e3feb0d67f26fbd2503fd26c739fcc9
bestanyzombie <156037200+bestanyzombie@users.noreply.github.com>
2025-02-18 13:57:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+//console.log(navigator.cookieEnables);
+//document.cookie = //Access Token From GitHub API;
 
+//console.log(document.cookie);
+
+function loginCookie(name, value, daysToLive){
+  const date = new Date();
+  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = '${name}=${value}; ${expires}; path=/'
+}
+
+loginCookie('token', 'ExAmPlE_tOkEn', 14);
+
+console.log(document.cookie);
