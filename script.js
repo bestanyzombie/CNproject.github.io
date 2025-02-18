4fcbc197b1278a62fad3465cf2068a1f61ac1819
bestanyzombie <156037200+bestanyzombie@users.noreply.github.com>
2025-02-18 14:02:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function loginCookie(name, value, daysToLive){
   document.cookie = '${name}=${value}; ${expires}; path=/'
 }
 
+function signoutCookie(name){
+  loginCookie(name, null, null);
+}
+
 loginCookie('token', 'ExAmPlE_tOkEn', 14);
 
 console.log(document.cookie);
+
+signoutCookie('token');
+
+console.log(document.cookie)
