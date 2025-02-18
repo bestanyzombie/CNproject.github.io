91bec23f613c6d420e2504d75256a2021d95f713
bestanyzombie <156037200+bestanyzombie@users.noreply.github.com>
2025-02-18 18:11:12 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,61 +5,11 @@
 <!DOCTYPE html>
 <html>
   <head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-KS475VE2S3"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-KS475VE2S3');
-    </script>
-    <title>Login</title>
   </head>
   <body>
-    
-    <style type="text/css">  
-      #text{
-
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-      </style>
-    
-    	<div id="box">
-		
-		<form method="post">
-			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
-
-			<input id="text" type="text" name="user_name"><br><br>
-			<input id="text" type="password" name="password"><br><br>
-
-			<input id="button" type="submit" value="Login"><br><br>
-
-			<a href="signup.php">Signup</a><br><br>
-		</form>
-	</div>
-    
+    <p>
+      GitHub API
+      <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=<%= client_id %>">Click here</a> to begin!
+    </p>
   </body>
 </html>
-
-# commit testing
