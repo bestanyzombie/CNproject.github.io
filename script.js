f0af5ad4022120bcdd0c48a14a559a00cd35e54b
bestanyzombie <156037200+bestanyzombie@users.noreply.github.com>
2025-02-27 13:13:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 
 //console.log(document.cookie);
 
+
+// <- LOGIN COOKIE -> //
 function loginCookie(name, value, daysToLive){
   const date = new Date();
   date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
@@ -14,6 +16,26 @@ function signoutCookie(name){
   loginCookie(name, null, null);
 }
 
+// <- LOGIN BUTTON + RETURN USER TOKEN -> //
+function App() {
+  .
+
+  function loginWithGithub() {
+  window.location.assign("https://github.com/login/oauth/authorize?client_id=Ov23liC4cxEBw5Hgys9Q");
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={loginWithGithub}>
+          Login with Github
+        </button>
+      </header>
+    </div>
+  );
+}
+
+// Testing
 loginCookie('token', 'ExAmPlE_tOkEn', 14);
 
 console.log(document.cookie);
